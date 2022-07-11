ca326af819dc594ba9e91b2be2317e4fa26128a1
Cory Metcalf <corymetcalf96@gmail.com>
2022-07-11 14:36:57 -0400

    Added seed data and index page functionality starting blocks

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     # PATH TO GO TO ABOUT PAGE
     path('about/', views.about, name='about'),
+    # PATH TO VIEW ALL APPLICATIONS
+    path('applications/', views.applications_index, name='applications_index'),
 ]
