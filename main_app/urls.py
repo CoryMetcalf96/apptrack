54387c6b7f2d5f449f653b7c99c358f004edd9a4
Cory Metcalf <corymetcalf96@gmail.com>
2022-07-11 11:54:29 -0400

    Added basic view templates and started mapping out CSS

---
+++ main_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # PATH TO GO HOME
+    path('', views.home, name='home'),
+    # PATH TO GO TO ABOUT PAGE
+    path('about/', views.about, name='about'),
+]
